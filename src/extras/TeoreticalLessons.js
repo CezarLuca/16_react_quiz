3b8e44192f6d2fcc1a5c68e95931e97dd9426f17
CezarLuca <71207294+CezarLuca@users.noreply.github.com>
2024-04-03 23:57:19 +0200

    Teoretical Lesson 1

---
+++ src/extras/TeoreticalLessons.js
@@ -0,0 +1,7 @@
+//----------------------------------------------------------------------------------------------
+/* Why useReducer?
+
+    Stete Management with useState is not enough in certain situations:
+        1. When components have a lot of state variables and state updates, spread across many 
+        event handlers all over the component.
+*/
