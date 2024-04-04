a0900edeed8b4823fbd3def3e78b82dc88c00d74
CezarLuca <71207294+CezarLuca@users.noreply.github.com>
2024-04-04 23:52:42 +0200

    Teoretical Lesson 3

--- src/extras/TeoreticalLessons.js
+++ src/extras/TeoreticalLessons.js
@@ -9,3 +9,13 @@
         3. When updating one piece of state depends on one or multiple other pieces of state.
     In all these cases, useReducer can be a better solution than useState.
 */
+//----------------------------------------------------------------------------------------------
+/* Managing State with useReducer:
+
+    1. An alternative way of setting state, ideal for complex state and related pieces of state.
+    2. Stores related pieces of state in a state object.
+    3. useReducer need reducer: function containing all logic to update state. Decouples state 
+        logic from component logic.
+    4. 
+    5. 
+*/
