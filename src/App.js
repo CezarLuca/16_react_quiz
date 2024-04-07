a27b39101c483142f77c6e05cb9729f602fc37ec
CezarLuca <71207294+CezarLuca@users.noreply.github.com>
2024-04-07 21:09:56 +0200

    The React Quiz App Introduction done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 // import DateCounter from "./DateCounter";
 import Header from "./Header";
+import Main from "./Main";
 
 export default function App() {
     return (
         <div className="app">
             {/* <DateCounter /> */}
             <Header />
-            <main className="main">
+            <Main>
                 <p>1/15</p>
                 <p>Question?</p>
-            </main>
+            </Main>
         </div>
     );
 }
