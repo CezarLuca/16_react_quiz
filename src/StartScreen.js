d2bec5f1fb42dfeaaabf88abdfc2ba85ba43f425
CezarLuca <71207294+CezarLuca@users.noreply.github.com>
2024-04-08 23:50:27 +0200

    Handling Loading Error and Ready Status part 2

---
+++ src/StartScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function StartScreen() {
+    return (
+        <div className="start">
+            <h2>Welcome to The React Quiz!</h2>
+            <p>X questions to test your React mastery.</p>
+            <button>Start Quiz</button>
+        </div>
+    );
+}
