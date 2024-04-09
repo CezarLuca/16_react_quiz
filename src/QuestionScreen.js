74fe2011e890b4fb43bc818ad9cc236cc2dd6286
CezarLuca <71207294+CezarLuca@users.noreply.github.com>
2024-04-09 23:51:20 +0200

    Displaying Questions Part 2

--- src/QuestionScreen.js
+++ src/QuestionScreen.js
@@ -1,5 +1,17 @@
 import React from "react";
 
-export default function QuestionScreen() {
-    return <div>Question</div>;
+export default function QuestionScreen({ question }) {
+    console.log(question);
+    return (
+        <div>
+            <h4>{question.question}</h4>
+            <div className="options">
+                {question.options.map((option) => (
+                    <button key={option} className="btn btn-option">
+                        {option}
+                    </button>
+                ))}
+            </div>
+        </div>
+    );
 }
