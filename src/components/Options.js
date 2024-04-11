cf7bca33739007785305460fea364693838db397
CezarLuca <71207294+CezarLuca@users.noreply.github.com>
2024-04-12 00:24:01 +0200

    Handling New Answers done

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,22 +1,46 @@
 import React from "react";
 
 export default function Options({ question, dispatch, answer }) {
+    const hasAnswered = answer !== null;
+
+    const getButtonClass = (index) => {
+        let className = "btn btn-option";
+
+        if (answer === index) {
+            className += " answer";
+        }
+
+        if (hasAnswered) {
+            if (index === question.correctOption) {
+                className += " correct";
+            } else {
+                className += " wrong";
+            }
+        }
+
+        return className;
+    };
+
     return (
         <div>
             <div className="options">
                 {question.options.map((option, index) => (
                     <button
                         key={option}
-                        className={`btn btn-option ${
-                            answer === index ? "answer" : ""
-                        } ${
-                            index === question.correctOption
-                                ? "correct"
-                                : "wrong"
-                        }`}
+                        // className={`btn btn-option ${
+                        //     answer === index ? "answer" : ""
+                        // } ${
+                        //     hasAnswered
+                        //         ? index === question.correctOption
+                        //             ? "correct"
+                        //             : "wrong"
+                        //         : ""
+                        // }`}
+                        className={getButtonClass(index)}
                         onClick={() =>
                             dispatch({ type: "newAnswer", payload: index })
                         }
+                        disabled={hasAnswered}
                     >
                         {option}
                     </button>
