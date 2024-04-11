72ed72fbd106c0d2e3b721eba90cf215abc44dc6
CezarLuca <71207294+CezarLuca@users.noreply.github.com>
2024-04-11 23:58:49 +0200

    Handling New Answers part 3 and bug fix

--- src/components/Options.js
+++ src/components/Options.js
@@ -7,7 +7,13 @@ export default function Options({ question, dispatch, answer }) {
                 {question.options.map((option, index) => (
                     <button
                         key={option}
-                        className="btn btn-option"
+                        className={`btn btn-option ${
+                            answer === index ? "answer" : ""
+                        } ${
+                            index === question.correctOption
+                                ? "correct"
+                                : "wrong"
+                        }`}
                         onClick={() =>
                             dispatch({ type: "newAnswer", payload: index })
                         }
