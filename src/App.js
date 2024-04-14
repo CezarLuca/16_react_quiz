60a6ffb7d73c1893f2ddc67545b0f57edd573875
CezarLuca <71207294+CezarLuca@users.noreply.github.com>
2024-04-14 21:38:05 +0200

    Finishing a Quiz done

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ const initialState = {
     index: 0,
     answer: null,
     points: 0,
+    highscore: 0,
 };
 
 function reducer(state, action) {
@@ -44,7 +45,14 @@ function reducer(state, action) {
                 answer: null,
             };
         case "finish":
-            return { ...state, status: "finished" };
+            return {
+                ...state,
+                status: "finished",
+                highscore:
+                    state.points > state.highscore
+                        ? state.points
+                        : state.highscore,
+            };
 
         default:
             throw new Error(`Unknown action type: ${action.type}`);
@@ -52,10 +60,8 @@ function reducer(state, action) {
 }
 
 export default function App() {
-    const [{ questions, status, index, answer, points }, dispatch] = useReducer(
-        reducer,
-        initialState
-    );
+    const [{ questions, status, index, answer, points, highscore }, dispatch] =
+        useReducer(reducer, initialState);
     useEffect(() => {
         fetch("http://localhost:9000/questions")
             .then((response) => response.json())
@@ -110,6 +116,7 @@ export default function App() {
                     <FinishScreen
                         points={points}
                         maxPossiblePoints={maxPossiblePoints}
+                        highscore={highscore}
                     />
                 )}
             </Main>
--- src/components/FinishScreen.js
+++ src/components/FinishScreen.js
@@ -1,12 +1,27 @@
 import React from "react";
 
-export default function FinishScreen({ points, maxPossiblePoints }) {
+export default function FinishScreen({ points, maxPossiblePoints, highscore }) {
     const percentage = (points / maxPossiblePoints) * 100;
 
+    let emoji;
+
+    if (percentage === 100) {
+        emoji = "🎉";
+    } else if (percentage >= 80) {
+        emoji = "👏";
+    } else if (percentage >= 60) {
+        emoji = "🙂";
+    } else {
+        emoji = "😢";
+    }
+
     return (
-        <p className="result">
-            You scored <strong>{points}</strong> out of {maxPossiblePoints} (
-            {Math.ceil(percentage)}%)
-        </p>
+        <>
+            <p className="result">
+                <span>{emoji}</span> You scored <strong>{points}</strong> out of{" "}
+                {maxPossiblePoints} ({Math.ceil(percentage)}%)
+            </p>
+            <p className="highscore">(Hishscore: {highscore} points)</p>
+        </>
     );
 }
