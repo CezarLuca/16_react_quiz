bc21a8c934ba7e3c0e0e2a3475556a68e1398ffc
CezarLuca <71207294+CezarLuca@users.noreply.github.com>
2024-04-15 23:49:01 +0200

    Setting Up a Timer with useEffect part 2

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,5 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function Timer() {
+    useEffect(() => {
+        setInterval(() => {
+            console.log("tick");
+        }, 1000);
+    }, []);
+
     return <div className="timer">05:00</div>;
 }
