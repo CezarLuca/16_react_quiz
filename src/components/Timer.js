d48a6e2b8948e27c239a02758e998e53925a68e7
CezarLuca <71207294+CezarLuca@users.noreply.github.com>
2024-04-15 23:58:34 +0200

    Added Time remaining Functionality

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,6 +1,9 @@
 import React, { useEffect } from "react";
 
 export default function Timer({ dispatch, secondsRemaining }) {
+    let minutesRemaining = Math.floor(secondsRemaining / 60);
+    let seconds = secondsRemaining % 60;
+
     useEffect(() => {
         setInterval(() => {
             // console.log("tick");
@@ -8,5 +11,10 @@ export default function Timer({ dispatch, secondsRemaining }) {
         }, 1000);
     }, [dispatch]);
 
-    return <div className="timer">{secondsRemaining}</div>;
+    return (
+        <div className="timer">
+            Time Left: {minutesRemaining}:
+            {seconds < 10 ? `0${seconds}` : seconds}
+        </div>
+    );
 }
