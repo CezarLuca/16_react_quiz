53af94a71ac9d5325d82e3f41d0236fceb76f0d5
CezarLuca <71207294+CezarLuca@users.noreply.github.com>
2024-06-30 23:57:55 +0200

    Refcatored StartScreen component to use Context API

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -1,10 +1,29 @@
-import React from "react";
+// import React from "react";
 
-export default function StartScreen({ numberOfQuestions, dispatch }) {
+// export default function StartScreen({ numberOfQuestions, dispatch }) {
+//     return (
+//         <div className="start">
+//             <h2>Welcome to The React Quiz!</h2>
+//             <h3>{numberOfQuestions} questions to test your React mastery.</h3>
+//             <button
+//                 className="btn btn-ui"
+//                 onClick={() => dispatch({ type: "start" })}
+//             >
+//                 Start Quiz
+//             </button>
+//         </div>
+//     );
+// }
+
+import { useQuiz } from "../contexts/QuizContext";
+
+function StartScreen() {
+    const { numQuestions, dispatch } = useQuiz();
+
     return (
         <div className="start">
             <h2>Welcome to The React Quiz!</h2>
-            <h3>{numberOfQuestions} questions to test your React mastery.</h3>
+            <h3>{numQuestions} questions to test your React mastery.</h3>
             <button
                 className="btn btn-ui"
                 onClick={() => dispatch({ type: "start" })}
@@ -14,3 +33,5 @@ export default function StartScreen({ numberOfQuestions, dispatch }) {
         </div>
     );
 }
+
+export default StartScreen;
