19a204c58d4c8fdfc619012e869739b3badf7558
CezarLuca <71207294+CezarLuca@users.noreply.github.com>
2024-06-30 23:59:19 +0200

    Refactored Timer component tp use Context API

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,12 +1,37 @@
-import React, { useEffect } from "react";
+// import React, { useEffect } from "react";
 
-export default function Timer({ dispatch, secondsRemaining }) {
+// export default function Timer({ dispatch, secondsRemaining }) {
+//     let minutesRemaining = Math.floor(secondsRemaining / 60);
+//     let seconds = secondsRemaining % 60;
+
+//     useEffect(() => {
+//         const id = setInterval(() => {
+//             // console.log("tick");
+//             dispatch({ type: "tick" });
+//         }, 1000);
+//         return () => clearInterval(id);
+//     }, [dispatch]);
+
+//     return (
+//         <div className="timer">
+//             Time Left:{" "}
+//             {minutesRemaining < 10 ? `0${minutesRemaining}` : minutesRemaining}:
+//             {seconds < 10 ? `0${seconds}` : seconds}
+//         </div>
+//     );
+// }
+
+import { useEffect } from "react";
+import { useQuiz } from "../contexts/QuizContext";
+
+function Timer() {
+    const { dispatch, secondsRemaining } = useQuiz();
+
     let minutesRemaining = Math.floor(secondsRemaining / 60);
     let seconds = secondsRemaining % 60;
 
     useEffect(() => {
         const id = setInterval(() => {
-            // console.log("tick");
             dispatch({ type: "tick" });
         }, 1000);
         return () => clearInterval(id);
@@ -20,3 +45,5 @@ export default function Timer({ dispatch, secondsRemaining }) {
         </div>
     );
 }
+
+export default Timer;
