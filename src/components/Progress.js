345f6d542599c90f356e5e54a1c014dc227a0166
CezarLuca <71207294+CezarLuca@users.noreply.github.com>
2024-06-30 21:39:30 +0200

    Setted up QuestionScreen for Context API

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,20 +1,42 @@
-import React from "react";
+// import React from "react";
 
-export default function Progress({
-    index,
-    numberOfQuestions,
-    points,
-    maxPossiblePoints,
-    answer,
-}) {
+// export default function Progress({
+//     index,
+//     numberOfQuestions,
+//     points,
+//     maxPossiblePoints,
+//     answer,
+// }) {
+//     return (
+//         <header className="progress">
+//             <progress
+//                 max={numberOfQuestions}
+//                 value={index + Number(answer !== null)}
+//             ></progress>
+//             <p>
+//                 Question <strong>{index + 1}</strong> / {numberOfQuestions}
+//             </p>
+//             <p>
+//                 <strong>{points}</strong> / {maxPossiblePoints} points
+//             </p>
+//         </header>
+//     );
+// }
+
+import { useQuiz } from "../contexts/QuizContext";
+
+function Progress() {
+    const { index, numQuestions, points, maxPossiblePoints, answer } =
+        useQuiz();
+
     return (
         <header className="progress">
             <progress
-                max={numberOfQuestions}
+                max={numQuestions}
                 value={index + Number(answer !== null)}
             ></progress>
             <p>
-                Question <strong>{index + 1}</strong> / {numberOfQuestions}
+                Question <strong>{index + 1}</strong> / {numQuestions}
             </p>
             <p>
                 <strong>{points}</strong> / {maxPossiblePoints} points
@@ -22,3 +44,5 @@ export default function Progress({
         </header>
     );
 }
+
+export default Progress;
--- src/components/QuestionScreen.js
+++ src/components/QuestionScreen.js
@@ -1,12 +1,31 @@
-import React from "react";
+// import React from "react";
+// import Options from "./Options";
+
+// export default function QuestionScreen({ question, dispatch, answer }) {
+//     // console.log(question);
+//     return (
+//         <div>
+//             <h4>{question.question}</h4>
+//             <Options question={question} dispatch={dispatch} answer={answer} />
+//         </div>
+//     );
+// }
+
+import { useQuiz } from "../contexts/QuizContext";
 import Options from "./Options";
 
-export default function QuestionScreen({ question, dispatch, answer }) {
-    // console.log(question);
+function QuestionScreen() {
+    const { questions, index } = useQuiz();
+    const question = questions.at(index);
+
+    console.log(question);
+
     return (
         <div>
             <h4>{question.question}</h4>
-            <Options question={question} dispatch={dispatch} answer={answer} />
+            <Options question={question} />
         </div>
     );
 }
+
+export default QuestionScreen;
