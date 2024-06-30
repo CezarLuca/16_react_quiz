55012f0ddbb665a16f00e18a8bff643c24552164
CezarLuca <71207294+CezarLuca@users.noreply.github.com>
2024-06-30 23:49:22 +0200

    Options refactored to use Context API

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,25 +1,61 @@
-import React from "react";
+// import React from "react";
 
-export default function Options({ question, dispatch, answer }) {
+// export default function Options({ question, dispatch, answer }) {
+//     const hasAnswered = answer !== null;
+
+//     const getButtonClass = (index) => {
+//         let className = "btn btn-option";
+
+//         if (answer === index) {
+//             className += " answer";
+//         }
+
+//         if (hasAnswered) {
+//             if (index === question.correctOption) {
+//                 className += " correct";
+//             } else {
+//                 className += " wrong";
+//             }
+//         }
+
+//         return className;
+//     };
+
+//     return (
+//         <div>
+//             <div className="options">
+//                 {question.options.map((option, index) => (
+//                     <button
+//                         key={option}
+//                         // className={`btn btn-option ${
+//                         //     answer === index ? "answer" : ""
+//                         // } ${
+//                         //     hasAnswered
+//                         //         ? index === question.correctOption
+//                         //             ? "correct"
+//                         //             : "wrong"
+//                         //         : ""
+//                         // }`}
+//                         className={getButtonClass(index)}
+//                         onClick={() =>
+//                             dispatch({ type: "newAnswer", payload: index })
+//                         }
+//                         disabled={hasAnswered}
+//                     >
+//                         {option}
+//                     </button>
+//                 ))}
+//             </div>
+//         </div>
+//     );
+// }
+
+import { useQuiz } from "../contexts/QuizContext";
+
+function Options({ question }) {
+    const { dispatch, answer } = useQuiz();
+
     const hasAnswered = answer !== null;
-
-    const getButtonClass = (index) => {
-        let className = "btn btn-option";
-
-        if (answer === index) {
-            className += " answer";
-        }
-
-        if (hasAnswered) {
-            if (index === question.correctOption) {
-                className += " correct";
-            } else {
-                className += " wrong";
-            }
-        }
-
-        return className;
-    };
 
     return (
         <div>
@@ -27,16 +63,15 @@ export default function Options({ question, dispatch, answer }) {
                 {question.options.map((option, index) => (
                     <button
                         key={option}
-                        // className={`btn btn-option ${
-                        //     answer === index ? "answer" : ""
-                        // } ${
-                        //     hasAnswered
-                        //         ? index === question.correctOption
-                        //             ? "correct"
-                        //             : "wrong"
-                        //         : ""
-                        // }`}
-                        className={getButtonClass(index)}
+                        className={`btn btn-option ${
+                            answer === index ? "answer" : ""
+                        } ${
+                            hasAnswered
+                                ? index === question.correctOption
+                                    ? "correct"
+                                    : "wrong"
+                                : ""
+                        }`}
                         onClick={() =>
                             dispatch({ type: "newAnswer", payload: index })
                         }
@@ -49,3 +84,5 @@ export default function Options({ question, dispatch, answer }) {
         </div>
     );
 }
+
+export default Options;
