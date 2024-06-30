971cbc590b795569188a3cfdf1255c31fb1ca119
CezarLuca <71207294+CezarLuca@users.noreply.github.com>
2024-06-30 23:55:31 +0200

    NextButton refactored to use Context API

--- src/components/NextButton.js
+++ src/components/NextButton.js
@@ -1,18 +1,50 @@
-import React from "react";
+// import React from "react";
 
-export default function NextButton({
-    dispatch,
-    answer,
-    index,
-    numberOfQuestions,
-}) {
+// export default function NextButton({
+//     dispatch,
+//     answer,
+//     index,
+//     numberOfQuestions,
+// }) {
+//     if (answer === null) {
+//         return (
+//             <button disabled className="btn btn-ui">
+//                 Next
+//             </button>
+//         );
+//     } else if (index < numberOfQuestions - 1) {
+//         return (
+//             <button
+//                 className="btn btn-ui"
+//                 onClick={() => dispatch({ type: "nextQuestion" })}
+//             >
+//                 Next
+//             </button>
+//         );
+//     } else {
+//         return (
+//             <button
+//                 className="btn btn-ui"
+//                 onClick={() => dispatch({ type: "finish" })}
+//             >
+//                 Finish
+//             </button>
+//         );
+//     }
+// }
+
+import { useQuiz } from "../contexts/QuizContext";
+
+function NextButton() {
+    const { dispatch, answer, index, questions } = useQuiz();
+
     if (answer === null) {
         return (
             <button disabled className="btn btn-ui">
                 Next
             </button>
         );
-    } else if (index < numberOfQuestions - 1) {
+    } else if (index < questions.length - 1) {
         return (
             <button
                 className="btn btn-ui"
@@ -32,3 +64,5 @@ export default function NextButton({
         );
     }
 }
+
+export default NextButton;
